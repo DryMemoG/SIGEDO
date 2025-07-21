36122bc5ba27d22caac16fe1289ed0487c7cef54
Guillermo Galindo <memo29sep@gmail.com>
2025-07-21 10:47:15 -0600

    Creation of projects

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.get('/', (_req, res) => {
+    res.send('¡Hola desde tu backend con TypeScript y Node.js!');
+});
+
+app.listen(PORT, () => {
+    console.log(`El proceso ${process.env.SERVICE_NAME} está corriendo en http://${process.env.DOMAIN}:${PORT}`);
+});
